cb6d3f293d6064c055997fc35cb285fccec5a6be
Berg72 <bergy450r@gmail.com>
2024-04-16 16:45:03 -0700

    -create mapview and list view -rename contentview to homeview -rename entry point to CitySights -add picker to homeview to select between mapview or listview

---
+++ CitySights App/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  CitySights App
+//
+//  Created by Mark Bergeson on 4/12/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
